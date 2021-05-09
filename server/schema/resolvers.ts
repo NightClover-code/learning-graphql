410d4554a080408ef12ed7221e50968f2bafef38
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-05-10 00:24:53 +0200

    added more types to schema

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -1,14 +1,23 @@
-//importing data
-import { books } from '../data';
-
+//importing utils
+import { books, authors } from '../data';
+import _ from 'lodash';
 //resolvers
 export const resolvers = {
   Query: {
     books: () => books,
+    authors: () => authors,
+    book: (_root: any, { id }: any) => _.find(books, { id }),
+    author: (_root: any, { id }: any) => _.find(authors, { id }),
+  },
+  Book: {
+    author: (_book: any) => _.find(authors, { id: _book.authorId }),
+  },
+  Author: {
+    books: (_author: any) => _.filter(books, { authorId: _author.id }),
   },
   Mutation: {
-    addBook(parent: any, args: any) {
-      const newBook = args;
+    addBook(parent: any, bookParams: any) {
+      const newBook = bookParams;
       books.push(newBook);
       return newBook;
     },
